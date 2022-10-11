99e38843f6d7e62c113caacfed7ec7e30b05d3d2
Rajat Kumar Mishra <rajatkumar.mishra@galaxyweblinks.in>
2022-10-11 15:42:02 +0530

    styleguide update

--- src/pages/styleguide/styleguide.js
+++ src/pages/styleguide/styleguide.js
@@ -4,12 +4,13 @@ import styles from './styleguide.module.scss'
 const Styleguide = ({ }) => {
     return (
         <div className={styles.wrapper}>
+            <h1>components:</h1>
             <div className={styles.checboxMain}>
                 <CheckBox labelName={"dfndfg"} />
             </div>
-            <div>
+            <div className={styles.btnMain}>
                 <Button size={"lg"} variant={"solidPrimary"}> Demo </Button>
-                <Button size={"lg"} variant={"disaled"}> Demo</Button>
+                <Button size={"lg"} variant={"disaled"}>      Demo</Button>
                 <Button size={"lg"} variant={"solidPrimary"}> Demo</Button>
                 <Button size={"sm01"} variant={"solidSecondary"}>Demo</Button>
                 <Button size={"md"} variant={"solidSecondary"}>Demo</Button>
