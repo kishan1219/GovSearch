015b827a7455d22077ced108fb16e9b708cba1be
Kushagra <kushagra.chouhan@galaxyweblinks.in>
2022-10-11 12:56:51 +0530

    create a search

---
+++ src/components/shared/search/search.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import './search.scss';
+import {
+  Input,
+  Icon
+} from "../index";
+import propTypes from 'prop-types'
+
+
+import { useState } from 'react';
+export default function Search({ type, placeholder,onClick,className }) {
+  return (
+    <div className=' search'>
+      
+      <Input className={className}placeholder={placeholder} />
+      <div className='rightIcon'><Icon type={type} onClick={onClick} />
+      </div>
+
+    </div >
+
+
+
+  );
+}
+Search.propTypes = {
+  // label: propTypes.string,
+  placeholder: propTypes.string,
+  className: propTypes.any,
+  type: propTypes.any,
+  onClick: propTypes.func,
+}
