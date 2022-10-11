075c8abc914a2aae806d3c8ba60e5f77388252f9
Kishan Gupta <kishankumar.gupta>
2022-10-11 12:22:48 +0530

    checkbox push

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Checkbox from '../../components/shared/checkbox'
 
  const Home = () => {
   return (
-    <div><h1>Home</h1></div>
+    <div><h1>Home</h1>
+    <Checkbox labelName={"bgdfbj"} /> </div>
   )
 }
 
