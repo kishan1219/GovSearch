e68b999250645a21ce42d9dcfc12e4cc90ed4d49
Kishan Gupta <kishankumar.gupta>
2022-10-11 17:13:29 +0530

    newsCard update

--- src/components/shared/newsCard/newsCard.js
+++ src/components/shared/newsCard/newsCard.js
@@ -9,8 +9,8 @@ className
   return (
     <div className={Styles.wrapper}>
     <div className={Styles.newsCardMain}>
-        <Image className={Styles} src='images/newsCardImage.png' alt='image' />
-        <Text strong='strong6' family='roboto' >FORCM Jason R. Dunn is the new Force Master Chief of Commander Navy Installations Command</Text>
+        <Image className={Styles.cardImage} src='images/newsCardImage.png' alt='image' />
+        <Text className={Styles.textPrimary} strong='strong6' family='roboto' >FORCM Jason R. Dunn is the new Force Master Chief of Commander Navy Installations Command</Text>
     </div>
     <Text className={Styles.textSecond} family='poppins'>He succeeds former Chief, FORCM Greg A. Vidaurri, in the position. He served as Tomahawk LPO ...</Text>
       
